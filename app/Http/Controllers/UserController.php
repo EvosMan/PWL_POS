93e6e0167a56f4ab45ae9bc0b3bcba5dbb4c3540
EvosMan <mochamadmunif71@gmail.com>
2024-03-07 15:18:02 +0700

    Pertemuan 4 - Praktikum 2.2

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,9 +9,8 @@ use Illuminate\Support\Facades\Hash;
 class UserController extends Controller
 {
     public function index(){
-        $user = UserModel::where('level_id', '>=', 1)->firstOr(function(){
-            abort(404);
-        });
+        $user = UserModel::where('username', 'manager9')->firstOrFail();
+
         return view('user', ['data' => $user]);
     }
 }
